2a763db3a8318227bcecd565a19c3bc315f2e911
p3t3r276 <p3t3r276@gmail.com>
2020-07-02 18:04:08 +0700

    refactor tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,19 +4,26 @@ import ReactDOM from "react-dom";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+  let container;
+  let customer;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  const render = component => ReactDOM.render(component, container);
+
   it("renders the customer first name", () => {
-    const customer = { firstName: "Ashley" };
-    const container = document.createElement("div");
+    customer = { firstName: "Ashley" };
     document.body.appendChild(container);
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Ashley");
   });
 
   it("renders another customer first name", () => {
-    const customer = { firstName: "Jordan" };
-    const container = document.createElement("div");
+    customer = { firstName: "Jordan" };
     document.body.appendChild(container);
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
   });
 });
